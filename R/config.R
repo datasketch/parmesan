7ebef4a1ba7cd5cfaf31febc3f1b869643e74160
jpmarindiaz <jpmarindiaz@gmail.com>
2020-02-27 15:12:29 -0500

    exported input_ids function

--- R/config.R
+++ R/config.R
@@ -26,7 +26,7 @@ parmesan_config <- function(inputs_params = NULL, inputs_layout = NULL, config_p
   list(layout = layout, inputs = inputs)
 }
 
-
+#' @export
 parmesan_input_ids <- function(section = NULL, config_path = "parmesan"){
   parmesan_config <- parmesan_config(config_path = config_path)
   if(is.null(section)){
@@ -39,6 +39,7 @@ parmesan_input_ids <- function(section = NULL, config_path = "parmesan"){
   unlist(unname(parmesan_config$layout))
 }
 
+#' @export
 parmesan_inputs <- function(config_path = "parmesan"){
   input_ids <- parmesan_input_ids(config_path = config_path)
   lapply(input_ids, function(i){
