f4dacf1b09b6ce6d3aa25572e8393e969bd9177b
brazadas <anaherand@hotmail.com>
2020-10-22 13:49:42 -0500

    updating parmesan infoTooltip function (as in shinypanels) finally -thanks to david- letting tooltip overlaps container, specially shinypanlels' panel

--- R/render_par_input_helpers.R
+++ R/render_par_input_helpers.R
@@ -43,6 +43,77 @@ input_has_show_if <- function(par_input){
 }
 
 
+# infoTooltip <- function(par_input, debug = FALSE) {
+#
+#   if(debug){
+#     message("in tooltip")
+#     str(par_input)
+#   }
+#
+#   icn <- par_input$input_info$icon %||% "info-circle"
+#   id <- par_input$id
+#   inp <- par_input$input_type
+#   title <- par_input$input_params$label
+#   info <- par_input$input_info$text
+#
+#   ic_a <- par_input$input_info$`icon-align` %||% "left"
+#   sl0 <- paste0(".control-label[for = '", id, "-selectized'] {position: relative;}")
+#   sl1 <- ""
+#   js <- "flex-start"
+#   if (ic_a == "right") {
+#     sl0 <- paste0(".control-label[for = '", id, "'] {width: 100%; position: relative;}")
+#     if (inp == "actionButton")
+#       # sl <- paste0("#", id, "{width: 100%;}")
+#       sl0 <- ""
+#     if (inp %in% c("selectInput", "selectizeInput"))
+#       sl0 <- paste0(".control-label[for = '", id, "-selectized'] {width: 100%; position: relative;}")
+#     if (inp == "checkboxInput")
+#       sl0 <- HTML(".checkbox > label {width: 100%;}")
+#
+#     js <- "space-between;"
+#   }
+#   if (inp == "checkboxInput") {
+#     sl1 <- HTML(paste0(sl1, ".checkbox > label > span {display: inline-block;}"))
+#   }
+#
+#   sl2 <- "
+#   .info-tool {
+#   display: inline-flex;
+#   }
+#   .tooltip-inf {
+#   cursor: pointer;
+#   position: relative;
+#   margin-left: 3px;
+#   }
+#   .tooltip-inf .tooltiptext {
+#   background: #eee;
+#   display: inline-block;
+#   font-size: 13px;
+#   left: 0;
+#   max-width: 200px;
+#   padding: 7px 10px;
+#   position: absolute;
+#   top: calc(100% + 5px);
+#   visibility: hidden;
+#   z-index: 100;
+#   }
+#   .info-tool:hover .tooltiptext {
+#   visibility: visible;
+#   } "
+#
+#   tagList(shiny::singleton(tags$head(tags$style(sl0))),
+#           shiny::singleton(tags$head(tags$style(sl1))),
+#           shiny::singleton(tags$head(tags$style(sl2))),
+#           HTML(paste0('<div style = "display: inline-flex; align-items: baseline; width: 100%; justify-content: ', js, '">',
+#                       title,
+#                       '<div class = "info-tool"> <div class="tooltip-inf">',
+#                       shiny::icon(icn),
+#                       '<span class = "tooltiptext" style = "font-weight: normal;">',
+#                       info,
+#                       '</span></div></div></div>')))
+# }
+
+
 infoTooltip <- function(par_input, debug = FALSE) {
 
   if(debug){
@@ -56,60 +127,48 @@ infoTooltip <- function(par_input, debug = FALSE) {
   title <- par_input$input_params$label
   info <- par_input$input_info$text
 
-  ic_a <- par_input$input_info$`icon-align` %||% "left"
-  sl0 <- paste0(".control-label[for = '", id, "-selectized'] {position: relative;}")
-  sl1 <- ""
-  js <- "flex-start"
-  if (ic_a == "right") {
-    sl0 <- paste0(".control-label[for = '", id, "'] {width: 100%; position: relative;}")
-    if (inp == "actionButton")
-      # sl <- paste0("#", id, "{width: 100%;}")
-      sl0 <- ""
-    if (inp %in% c("selectInput", "selectizeInput"))
-      sl0 <- paste0(".control-label[for = '", id, "-selectized'] {width: 100%; position: relative;}")
-    if (inp == "checkboxInput")
-      sl0 <- HTML(".checkbox > label {width: 100%;}")
-
-    js <- "space-between;"
-  }
-  if (inp == "checkboxInput") {
-    sl1 <- HTML(paste0(sl1, ".checkbox > label > span {display: inline-block;}"))
+  sl0 <- "
+  .tooltip0 {
+    align-items: center;
+    display: flex;
   }
 
-  sl2 <- "
-  .info-tool {
-  display: inline-flex;
+  .tooltip0-slot {
+    margin-left: 4px;
   }
-  .tooltip-inf {
-  cursor: pointer;
-  position: relative;
-  margin-left: 3px;
-  }
-  .tooltip-inf .tooltiptext {
-  background: #eee;
-  display: inline-block;
-  font-size: 13px;
-  left: 0;
-  max-width: 200px;
-  padding: 7px 10px;
-  position: absolute;
-  top: calc(100% + 5px);
-  visibility: hidden;
-  z-index: 100;
-  }
-  .info-tool:hover .tooltiptext {
-  visibility: visible;
-  } "
 
+  .tooltip0-icon {
+    color: #8097a4;
+    cursor: pointer;
+  }
+
+  .tooltip0-content {
+    background: #ffffff;
+    border: 1px solid #e6eaed;
+    box-shadow: 0 1px 10px 0 rgba(0, 0, 0, 0.1);
+    color: #435b69;
+    display: none;
+    font-family: 'IBM Plex Sans', sans-serif;
+    font-size: 0.75rem
+    font-size: 13px;
+    max-width: 250px;
+    min-width: 180px;
+    padding: 1rem;
+    position: absolute;
+  }
+
+  .tooltip0-slot:hover .tooltip0-content {
+    display: block;
+    z-index: 10;
+  }
+  "
   tagList(shiny::singleton(tags$head(tags$style(sl0))),
-          shiny::singleton(tags$head(tags$style(sl1))),
-          shiny::singleton(tags$head(tags$style(sl2))),
-          HTML(paste0('<div style = "display: inline-flex; align-items: baseline; width: 100%; justify-content: ', js, '">',
-                      title,
-                      '<div class = "info-tool"> <div class="tooltip-inf">',
+          HTML(paste0('<div class="tooltip0">',
+                      '<span>', title, '</span>',
+                      '<div class="tooltip0-slot"><span class="tooltip0-icon">',
                       shiny::icon(icn),
-                      '<span class = "tooltiptext" style = "font-weight: normal;">',
+                      '</span><div class="tooltip0-content" style = "font-weight: normal;">',
                       info,
-                      '</span></div></div></div>')))
-}
+                      '</div></div></div>')))
 
+}
