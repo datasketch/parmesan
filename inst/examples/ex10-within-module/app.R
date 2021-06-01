a5c7a85227487e4c0a2ab4b89d3c2ae4e928298d
Lena Mangold <lena@mangold.org>
2021-06-01 03:37:40 -0500

    update within module example

--- inst/examples/ex10-within-module/app.R
+++ inst/examples/ex10-within-module/app.R
@@ -45,7 +45,6 @@ parmServer <- function(id, r) {
       plot_type_selected <- reactive({
         req(plot_type_choices())
         plot_type_choices()[2]
-        "blahblah"
       })
 
       bins_default_value <- reactive({
