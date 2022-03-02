d2a582ce9512f0e5684f7416a4ae3b9666f6d855
Camila Achuri <camilaachury.4@gmail.com>
2022-03-01 20:41:25 -0500

    add reset inputs

---
+++ R/reset_inputs.R
@@ -0,0 +1,30 @@
+#'@export
+resetButtonUI <- function(id = "reset_button", icon_name = "" ,class = "btn action-button") {
+  ns <- shiny::NS(id)
+  shiny::actionButton(inputId = ns("resetButton"),
+                      label = " ",
+                      icon = icon(icon_name),
+                      class = class)
+}
+
+#' @export
+resetButtonServer <- function(session, input, id, id_reset = "all") {
+
+  ns <- session$ns
+  buttonId <- paste0(id, "-", ns("resetButton"))
+
+  observeEvent(input[[buttonId]], {
+
+    df_inputs <- parmesan:::initial_inputs_namespace(parmesan_inputs())
+
+    if (!("all" %in% id_reset)) {
+      if (sum(id_reset %in% df_inputs$id) == 0) stop("input not found in parmesan list")
+      df_inputs <- df_inputs %>% dplyr::filter(id %in% id_reset)
+    }
+
+    parmesan:::updateInput_function(session, df_inputs = df_inputs)
+
+  })
+
+
+}
